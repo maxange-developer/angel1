35a4b5902d87e429e31b2ab22f4d375fe12f9439
max <massiangelone73@gmail.com>
2026-05-16 20:12:45 +0100

    fix(journal): article cover photo objectPosition 30% → 45%

--- src/pages/journal/[slug].tsx
+++ src/pages/journal/[slug].tsx
@@ -60,7 +60,7 @@ export default function JournalSlug({
               alt={fm.title}
               fill
               sizes="100vw"
-              style={{ objectFit: "cover", objectPosition: "center 15%" }}
+              style={{ objectFit: "cover", objectPosition: "center 45%" }}
               priority
             />
           )}
